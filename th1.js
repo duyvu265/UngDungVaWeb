aec30bef61f71f38745839b4a4a1f5ed2f178799
duy thanh <you@example.com>
2023-03-20 14:13:18 +0700

    th2

--- th1.js
+++ th1.js
@@ -1,6 +1,7 @@
 let http=require('http');
 let sever=http.createServer((req, res)=>{
-    res.write('<h1>hello everybody</h1><hr>');
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write('<h5 style="color: gold">hello everybody</h5><hr>');
     res.end();
 });
 sever.listen(8080,'localhost');
